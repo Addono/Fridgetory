ffeb8926a78cac8eb59f3b85a4d63ff12ecef23d
Adriaan Knapen <hi@aknapen.nl>
2020-05-17 13:00:57 +0200

    build(apollo): add missing dependency for apollo client to fix fetch issue on build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import '../styles.css'
+import fetch from 'node-fetch'
 import {ApolloClient, ApolloProvider, HttpLink, InMemoryCache} from '@apollo/client'
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
         uri: '/api/graphql',
+// @ts-ignore
+        fetch: fetch,
     })
 })
 
