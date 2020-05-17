dc4ddd934c7d8cd71a7c62c07a8de5d917725bec
Adriaan Knapen <hi@aknapen.nl>
2020-05-17 17:31:32 +0200

    refactor(place): break out editable title functionality in seperate component

---
+++ components/EditableTitle.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+import { Divider, Input, Tooltip } from "antd";
+import { EditOutlined, SaveFilled, DeleteTwoTone } from "@ant-design/icons";
+
+export const EditableTitle = ({
+  name,
+  setName,
+  onDelete,
+}: {
+  name: string;
+  setName: (name: string) => void;
+  onDelete: () => void;
+}) => {
+  const [editing, setEditing] = useState<boolean>(false);
+  const [newName, setNewName] = useState<string>(name);
+
+  if (editing) {
+    const save = () => {
+      setName(newName);
+      setEditing(false);
+    };
+
+    const SaveButton = () => (
+      <Tooltip title="Save">
+        <SaveFilled onClick={save} />
+      </Tooltip>
+    );
+
+    const DeleteButton = () => (
+      <Tooltip title={"Delete"}>
+        <DeleteTwoTone twoToneColor={"red"} onClick={() => onDelete()} />
+      </Tooltip>
+    );
+
+    return (
+      <Input
+        defaultValue={name}
+        value={newName}
+        onChange={({ target: { value } }) => setNewName(value)}
+        onPressEnter={save}
+        addonBefore={<DeleteButton />}
+        addonAfter={<SaveButton />}
+      />
+    );
+  } else {
+    const EditButton = () => (
+      <Tooltip title="Edit">
+        <EditOutlined onClick={() => setEditing(true)} />
+      </Tooltip>
+    );
+
+    return (
+      <>
+        {name}
+        <Divider type={"vertical"} />
+        <EditButton />
+      </>
+    );
+  }
+};
--- components/Place/index.tsx
+++ components/Place/index.tsx
@@ -1,94 +1,61 @@
-import {Card, Divider, Input, Tooltip} from 'antd';
+import { Card, Divider } from "antd";
 
-import {Product, QUERY_PLACES} from "../Places";
+import { gql, useMutation } from "@apollo/client";
+
+import { Product, QUERY_PLACES } from "../Places";
 import AddProduct from "../AddProduct";
 import Items from "../Items";
-import {useState} from "react";
-import {EditOutlined, SaveFilled, DeleteTwoTone} from "@ant-design/icons"
-import {gql, useMutation} from "@apollo/client";
+import { EditableTitle } from "../EditableTitle";
 
 const MUTATION_EDIT_NAME_FOR_PLACE = gql`
-    mutation EditNameForPlace($name: String!, $id: Int!) {
-        updateOnePlace(
-            where: {id: $id}
-            data: {name: $name}
-        ) {
-            id
-        }
+  mutation EditNameForPlace($name: String!, $id: Int!) {
+    updateOnePlace(where: { id: $id }, data: { name: $name }) {
+      id
     }
-`
+  }
+`;
 
 const MUTATION_DELETE_PLACE = gql`
-    mutation DeletePlace($id: Int!) {
-        deleteOnePlace(where: {id: $id}) {
-            id
-        }
+  mutation DeletePlace($id: Int!) {
+    deleteOnePlace(where: { id: $id }) {
+      id
     }
-`
+  }
+`;
 
-const Title = ({name, id}: { name: string, id: number }) => {
-    const [editing, setEditing] = useState<boolean>(false)
-    const [newName, setNewName] = useState<string>(name)
+const Title = ({ name, id }: { name: string; id: number }) => {
+  const [setNameMutation] = useMutation(MUTATION_EDIT_NAME_FOR_PLACE, {
+    refetchQueries: [{ query: QUERY_PLACES }],
+  });
+  const [deletePlace] = useMutation(MUTATION_DELETE_PLACE, {
+    refetchQueries: [{ query: QUERY_PLACES }],
+  });
 
-    const [setNameMutation] = useMutation(MUTATION_EDIT_NAME_FOR_PLACE, {refetchQueries: [{query: QUERY_PLACES}]})
-    const [deletePlace] = useMutation(MUTATION_DELETE_PLACE, {refetchQueries: [{query: QUERY_PLACES}], variables: {id}})
+  return (
+    <EditableTitle
+      name={name}
+      setName={(name) => setNameMutation({ variables: { name, id } })}
+      onDelete={() => deletePlace({ variables: { id } })}
+    />
+  );
+};
 
-    if (editing) {
-        const save = () => {
-            setNameMutation({variables: {name: newName, id}})
-            setEditing(false)
-        }
+const Place = ({
+  id,
+  name,
+  products,
+}: {
+  id: number;
+  name: string;
+  products: Product[];
+}) => (
+  <Card title={<Title name={name} id={id} />} style={{ width: "100%" }}>
+    {products.map(({ id, items, productType: { name } }) => (
+      <Items productId={id} key={name} name={name} items={items} />
+    ))}
+    <Divider />
+    <AddProduct placeId={id} existingProducts={products} />
+  </Card>
+);
 
-        const SaveButton = () => (
-            <Tooltip title="Save">
-                <SaveFilled onClick={save} />
-            </Tooltip>
-        )
-
-        const DeleteButton = () => (
-            <Tooltip title={"Delete"}>
-                <DeleteTwoTone twoToneColor={"red"} onClick={() => deletePlace()} />
-            </Tooltip>
-        )
-
-        return (
-            <Input
-                defaultValue={name}
-                value={newName}
-                onChange={({target: {value}}) => setNewName(value)}
-                onPressEnter={save}
-                addonBefore={<DeleteButton/>}
-                addonAfter={<SaveButton/>}
-            />
-        )
-    } else {
-        const EditButton = () => (
-            <Tooltip title="Edit">
-                <EditOutlined onClick={() => setEditing(true)}/>
-            </Tooltip>
-        )
-
-        return (
-            <>
-                {name}
-                <Divider type={"vertical"}/>
-                <EditButton/>
-            </>
-        )
-    }
-}
-
-const Place = ({id, name, products}: { id: number, name: string, products: Product[] }) => (
-    <Card title={<Title name={name} id={id}/>} style={{width: "100%"}}>
-        {products.map(({id, items, productType: {name}}) =>
-            <Items productId={id}
-                   key={name}
-                   name={name}
-                   items={items}
-            />)}
-        <Divider/>
-        <AddProduct placeId={id} existingProducts={products}/>
-    </Card>
-)
-
-export default Place
+export default Place;
