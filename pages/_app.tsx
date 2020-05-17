9c65308a262c1304b48f82a510eda93c2bae5920
Adriaan Knapen <hi@aknapen.nl>
2020-05-17 15:40:47 +0200

    feat(deploy): allow setting the graphql endpoint host using the GRAPHQL_HOST environment variable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,13 @@ import '../styles.css'
 import fetch from 'node-fetch'
 import {ApolloClient, ApolloLink, ApolloProvider, createHttpLink, InMemoryCache} from '@apollo/client'
 
+const GRAPHQL_HOST = process.env.GRAPHQL_HOST ?? ""
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: ApolloLink.from([
         createHttpLink({
-            uri: '/api/graphql',
+            uri: `${GRAPHQL_HOST}/api/graphql`,
 // @ts-ignore
             fetch: fetch,
         })
