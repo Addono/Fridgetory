cad690d090e9d3b6018da1003bda14721c9c5730
Adriaan Knapen <hi@aknapen.nl>
2020-10-31 13:01:24 +0100

    refactor: adopt @nexus/schema instead of nexus

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,67 +1,79 @@
-import { use, schema } from 'nexus'
-import { prisma } from 'nexus-plugin-prisma'
+import { queryType, mutationType, objectType, makeSchema } from '@nexus/schema'
+import { nexusPrisma } from 'nexus-plugin-prisma'
 
-use(prisma({ features: { crud: true } }))
-
-schema.queryType({
-  definition: (t) => {
-    t.crud.product()
-    t.crud.products()
-    t.crud.productType()
-    t.crud.productTypes()
-    t.crud.item()
-    t.crud.items()
-    t.crud.place()
-    t.crud.places()
+export const schema = makeSchema({
+  plugins: [
+    nexusPrisma({
+      experimentalCRUD: true,
+    }),
+  ],
+  typegenAutoConfig: {
+    contextType: '{ prisma: PrismaClient.PrismaClient }',
+    sources: [{ source: '.prisma/client', alias: 'PrismaClient' }],
   },
-})
+  outputs: {},
+  types: [
+    queryType({
+      definition: (t) => {
+        t.crud.product()
+        t.crud.products()
+        t.crud.productType()
+        t.crud.productTypes()
+        t.crud.item()
+        t.crud.items()
+        t.crud.place()
+        t.crud.places()
+      },
+    }),
 
-schema.mutationType({
-  definition: (t) => {
-    t.crud.createOneProduct()
-    t.crud.createOneProductType()
-    t.crud.createOneItem()
-    t.crud.createOnePlace()
-    t.crud.updateOnePlace()
-    t.crud.updateOneProductType()
-    t.crud.deleteOneItem()
-    t.crud.deleteOnePlace()
-    t.crud.deleteOneProduct()
-  },
-})
+    mutationType({
+      definition: (t) => {
+        t.crud.createOneProduct()
+        t.crud.createOneProductType()
+        t.crud.createOneItem()
+        t.crud.createOnePlace()
+        t.crud.updateOnePlace()
+        t.crud.updateOneProductType()
+        t.crud.deleteOneItem()
+        t.crud.deleteOnePlace()
+        t.crud.deleteOneProduct()
+      },
+    }),
 
-schema.objectType({
-  name: 'ProductType',
-  definition: (t) => {
-    t.model.id()
-    t.model.name()
-  },
-})
+    objectType({
+      name: 'ProductType',
+      definition: (t) => {
+        t.model.id()
+        t.model.name()
+      },
+    }),
 
-schema.objectType({
-  name: 'Product',
-  definition: (t) => {
-    t.model.id()
-    t.model.productType()
-    t.model.items()
-  },
-})
+    objectType({
+      name: 'Product',
+      definition: (t) => {
+        t.model.id()
+        t.model.productType()
+        t.model.items()
+      },
+    }),
 
-schema.objectType({
-  name: 'Item',
-  definition: (t) => {
-    t.model.id()
-    t.model.quantity()
-    t.model.unit()
-    t.model.product()
-  },
-})
+    objectType({
+      name: 'Item',
+      definition: (t) => {
+        t.model.id()
+        t.model.quantity()
+        t.model.unit()
+        t.model.product()
+      },
+    }),
 
-schema.objectType({
-  name: 'Place',
-  definition: (t) => {
-    t.model.id()
-    t.model.name()
-    t.model.products()
-  },
+    objectType({
+      name: 'Place',
+      definition: (t) => {
+        t.model.id()
+        t.model.name()
+        t.model.products()
+      },
+    }),
+  ],
 })
