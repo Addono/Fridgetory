4638dde743cb61dadc186f5852562557c4402540
Adriaan Knapen <hi@aknapen.nl>
2020-11-20 12:22:21 +0100

    feat(footer): shows version number at the bottom of the page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
+  env: {
+    version: require('./package.json').version,
+  },
   pwa: {
     dest: 'public',
     runtimeCaching,
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -52,55 +52,75 @@ const ProvideApollo = ({ children }: { children: React.ReactNode }) => {
   return <ApolloProvider client={client}>{children}</ApolloProvider>
 }
 
-const _app = ({ Component, pageProps }: any) => (
-  <SharedSearchProvider>
-    <Layout className={'layout'}>
-      <Head>
-        <title>Fridgetory</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
+const _app = ({ Component, pageProps }: any) => {
+  // Get the version of the application from the environment variables
+  const { version } = process.env
 
-      <Layout.Header style={{ height: 64, position: 'fixed', zIndex: 1, width: '100%', padding: '0 2em' }}>
-        <Row style={{ flexFlow: 'row' }}>
-          <Col flex={'content'} style={{ color: 'white', paddingRight: '1em', fontWeight: 'bold' }}>
-            Fridgetory
-          </Col>
-          <Col flex={'auto'}>
-            <SearchInput />
-          </Col>
-        </Row>
-      </Layout.Header>
+  return (
+    <>
+      <SharedSearchProvider>
+        <Layout className={'layout'}>
+          <Head>
+            <title>Fridgetory</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+            <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+            <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+            <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+            <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+            <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+            <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+            <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+            <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+            <meta name="theme-color" content="#ffffff" />
+          </Head>
 
-      <Layout.Content style={{ marginTop: 64 }}>
-        <ProvideApollo>
-          <Component {...pageProps} />
-        </ProvideApollo>
-      </Layout.Content>
+          <Layout.Header style={{ height: 64, position: 'fixed', zIndex: 1, width: '100%', padding: '0 2em' }}>
+            <Row style={{ flexFlow: 'row' }}>
+              <Col flex={'content'} style={{ color: 'white', paddingRight: '1em', fontWeight: 'bold' }}>
+                Fridgetory
+              </Col>
+              <Col flex={'auto'}>
+                <SearchInput />
+              </Col>
+            </Row>
+          </Layout.Header>
 
-      <Layout.Footer style={{ textAlign: 'center' }}>
-        <a href={'https://aknapen.nl'} target="_blank" rel="noopener noreferrer" style={{ color: 'grey' }}>
-          2020&nbsp;©&nbsp;Adriaan Knapen
-        </a>
-      </Layout.Footer>
-    </Layout>
-  </SharedSearchProvider>
-)
+          <Layout.Content style={{ marginTop: 64 }}>
+            <ProvideApollo>
+              <Component {...pageProps} />
+            </ProvideApollo>
+          </Layout.Content>
+
+          <Layout.Footer style={{ textAlign: 'center' }}>
+            <a href={'https://aknapen.nl'} target="_blank" rel="noopener noreferrer" style={{ color: 'grey' }}>
+              2020&nbsp;©&nbsp;Adriaan Knapen
+            </a>
+            {version && (
+              <>
+                -
+                <a
+                  href={`https://github.com/Addono/Fridgetory/releases/tag/v${version}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: 'grey' }}
+                >
+                  v{version}
+                </a>
+              </>
+            )}
+          </Layout.Footer>
+        </Layout>
+      </SharedSearchProvider>
+    </>
+  )
+}
 
 export default _app
