834bd6238fd044ff5a2d627a8d7e1aff950e51fa
Adriaan Knapen <hi@aknapen.nl>
2020-11-24 16:51:47 +0100

    fix(style): synchronizes loading indicator styles

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,6 +3,19 @@ import { LoadingOutlined } from '@ant-design/icons'
 
 const spinnerIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />
 
-const Loading = () => <Spin indicator={spinnerIcon} />
+const Loading = ({ center = false, marginTop = false }: { center?: boolean; marginTop?: boolean }) => {
+  const style = {
+    ...(center && { display: 'grid', placeItems: 'center', width: '100%' }),
+    ...(marginTop && { marginTop: '3em' }),
+  }
+
+  return (
+    <>
+      <div style={style}>
+        <Spin indicator={spinnerIcon} />
+      </div>
+    </>
+  )
+}
 
 export default Loading
--- components/Places/index.tsx
+++ components/Places/index.tsx
@@ -80,11 +80,7 @@ const Places = () => {
 
   // Show a loading indicator
   if (loading) {
-    return (
-      <div style={{ display: 'grid', placeItems: 'center', width: '100%', marginTop: '3em' }}>
-        <Loading />
-      </div>
-    )
+    return <Loading center marginTop />
   }
 
   // Handle the case where there was no data and an error was emitted
