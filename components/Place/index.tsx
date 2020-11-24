c0b3012c66eae4454a2797679f698b4ded6b005a
Adriaan Knapen <hi@aknapen.nl>
2020-11-24 14:22:05 +0100

    feat: reduces spacing between items

--- components/Place/index.tsx
+++ components/Place/index.tsx
@@ -96,13 +96,15 @@ const Place = ({ id, name, products }: { id: number; name: string; products: Pro
   return (
     <Card data-cy={'place'} title={<Title name={name} id={id} canDelete={canDelete} />}>
       <Space direction={'vertical'} style={{ width: '100%' }}>
-        {visibleProducts
-          // Sort the products based on their name
-          .sort(sortProductByName)
-          // Render each product
-          .map(({ id, items, productType: { name } }) => (
-            <Items productId={id} key={name} name={name} items={items} />
-          ))}
+        <div>
+          {visibleProducts
+            // Sort the products based on their name
+            .sort(sortProductByName)
+            // Render each product
+            .map(({ id, items, productType: { name } }) => (
+              <Items productId={id} key={name} name={name} items={items} />
+            ))}
+        </div>
         {searchQuery.length > 0 ? (
           // Show the amount of hidden items when we are searching
           <HiddenItemsText amount={hiddenProductCount} />
